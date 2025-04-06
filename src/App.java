b0eb3e53956b2ed36c0f94b3b21e14e0ba2bf689
Felipe Cruz <felp.cruz.comp@gmail.com>
2025-04-06 17:31:44 -0300

    Recrusive Program

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Recursivo r = new Recursivo();
+        
+        System.out.println(r.triplica(3));
+
+        System.out.println(r.triplicaNaoEquivalente(3));
     }
 }
--- src/Recursivo.java
+++ src/Recursivo.java
@@ -1,3 +1,32 @@
 public class Recursivo {
+
+    public int triplica(int n){
+
+        if (n!= 0){
+            n--;
+            n = triplica(n);
+            n++;
+            n++;
+            n++;
+
+        } 
+        return n;
+    }
+
     
+
+    public int triplicaNaoEquivalente(int n){
+        if (n!= 0){
+            n--;
+            n = triplica(n);
+            n++;
+            n++;
+            n--;
+            n++;
+            n++;
+
+        } 
+        return n;
+    }
 }
+  
