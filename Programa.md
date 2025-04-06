24bc24864566b289a88ab25e2642932f42651579
JM <joaomiguel.semblan@gmail.com>
2025-04-06 18:45:42 -0300

    Fix programa monolitico 2

--- Programa.md
+++ Programa.md
@@ -12,11 +12,9 @@ Entrada: (R, ✓, 3)
 ### MONOLÍTICO
 
 - Triplica R
-R1: ler (n) vá para R2
-R2: Faça k = n*3
-R3: Se n==0 então vá para Rx, senão vá para R4
-R4: Faça n = n-1 então vá para R5
-R5: Faça n = n+1 então vá para R6
-R6: Faça n = n+1 então vá para R7
-R7: Faça n = n+1 então vá para R8
-R8: Faça n == k então vá para Rx, senão vá para R3
+R1: ler (a) vá para R2
+R2: Se a==0 então vá para Rx, senão vá para R3
+R3: Faça a_sub então vá para R4
+R4: Faça b_add então vá para R5
+R5: Faça b_add então vá para R6
+R6: Faça b_add então vá para R2
--- src/Monolitico.java
+++ src/Monolitico.java
@@ -1,14 +1,20 @@
 public class Monolitico {
-    private int input;
-    private int n;
-    private int k;
+    private int a;
+    private int b;
 
     
 
-    public Monolitico(int input) {
-        this.input = input;
-        this.n = 0;
-        this.k = 0;
+    public Monolitico(int a) {
+        this.a = a;
+        this.b = 0;
+    }
+
+    public void a_sub(){
+        setA(getA()-1);
+    }
+
+    public void b_add(){
+        setB(getB()+1);
     }
 
     public void Rodar(){
@@ -16,67 +22,49 @@ public class Monolitico {
     }
 
     public void R1(){
-        setN(getInput());
         R2();
     }
     public void R2(){
-        setK(getInput()*3);
-        R3();
-    }
-    public void R3(){
-        if (getN()==0){
-            System.out.println("Fim do programa");
-        }else{
-            R4();
-        }
-    }
-    public void R4(){
-        setN(getN()-1);
-        R5();
-    }
-    public void R5(){
-        setN(getN()+1);
-        R6();
-    }
-    public void R6(){
-        setN(getN()+1);
-        R7();
-    }
-    public void R7(){
-        setN(getN()+1);
-        R8();
-    }
-    public void R8(){
-        if (getN() == getK()) {
-            System.out.println("Fim do programa " + getN());
+        if(getA()==0){
+            System.out.println("Fim do programa! " + getB());
         }else{
             R3();
         }
     }
-
-    public int getInput() {
-        return input;
+    public void R3(){
+            a_sub();
+            R4();
+    }
+    public void R4(){
+        b_add();
+        R5();
+    }
+    public void R5(){
+        b_add();
+        R6();
+    }
+    public void R6(){
+        b_add();
+        R2();
     }
 
-    public void setInput(int input) {
-        this.input = input;
+
+    public int getA() {
+        return a;
     }
 
-    public int getK() {
-        return k;
+    public void setA(int a) {
+        this.a = a;
     }
 
-    public void setK(int k) {
-        this.k = k;
+    public int getB() {
+        return b;
     }
 
-    public int getN() {
-        return n;
+    public void setB(int b) {
+        this.b = b;
     }
 
-    public void setN(int n) {
-        this.n = n;
-    }
-
+    
     
 }
