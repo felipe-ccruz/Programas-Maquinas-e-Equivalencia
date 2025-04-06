785967cb525384fc2fc12d5ee43c38d8f3b2a57a
JM <joaomiguel.semblan@gmail.com>
2025-04-06 18:00:35 -0300

    Add programa monolitico

--- Programa.md
+++ Programa.md
@@ -9,10 +9,14 @@ Entrada: (R, ✓, 3)
 
 (se zero então ✓ senão (sub; R; add; add; add), ✓, 3)
 ```
+### MONOLÍTICO
 
-
-
-
-
-
-
+- Triplica R
+R1: ler (n) vá para R2
+R2: Faça k = n*3
+R3: Se n==0 então vá para Rx, senão vá para R4
+R4: Faça n = n-1 então vá para R5
+R5: Faça n = n+1 então vá para R6
+R6: Faça n = n+1 então vá para R7
+R7: Faça n = n+1 então vá para R8
+R8: Faça n == k então vá para Rx, senão vá para R3
--- src/Monolitico.java
+++ src/Monolitico.java
@@ -1,3 +1,85 @@
 public class Monolitico {
     
 }
+public class Monolitico {
+    private int input;
+    private int n;
+    private int k;
+
+    
+
+    public Monolitico(int input) {
+        this.input = input;
+        this.n = 0;
+        this.k = 0;
+    }
+
+    public void Rodar(){
+        R1();
+    }
+
+    public void R1(){
+        setN(getInput());
+        R2();
+    }
+    public void R2(){
+        setK(getInput()*3);
+        R3();
+    }
+    public void R3(){
+        if (getN()==0){
+            System.out.println("Fim do programa");
+        }else{
+            R4();
+        }
+    }
+    public void R4(){
+        setN(getN()-1);
+        R5();
+    }
+    public void R5(){
+        setN(getN()+1);
+        R6();
+    }
+    public void R6(){
+        setN(getN()+1);
+        R7();
+    }
+    public void R7(){
+        setN(getN()+1);
+        R8();
+    }
+    public void R8(){
+        if (getN() == getK()) {
+            System.out.println("Fim do programa " + getN());
+        }else{
+            R3();
+        }
+    }
+
+    public int getInput() {
+        return input;
+    }
+
+    public void setInput(int input) {
+        this.input = input;
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public void setK(int k) {
+        this.k = k;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    
+}
