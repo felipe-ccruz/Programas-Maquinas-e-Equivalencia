aae1cf0eb6e606a7ab3806f942ca0ec073daeb3f
Felipe Cruz <felp.cruz.comp@gmail.com>
2025-04-23 21:34:23 -0300

    Correção dos códigos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // IGNORE
+        Iterativo it = new Iterativo();
+        System.out.println(it.triplica());
+        
+        System.out.println("\n\n");
+
+        Recursivo r = new Recursivo();
+        System.out.println(r.triplica(3));
     }
 }
--- src/Recursivo.java
+++ src/Recursivo.java
@@ -2,43 +2,25 @@ public class Recursivo {
     public int triplica(int n){
         System.out.println("\n\n-----------PROGRAMA---RECUSIVO-----------\n\n");
 
+        int value = R(n);
+
+        System.out.print("Saída: ");
+        return value;
+    }
+
+    public int R(int n){
         int a = n;
         int b = 0;
          
         if (a!= 0){
             a--;
-            b = triplica(a);
+            b = R(a);
             b++;
             b++;
             b++;
 
         } 
-
-        System.out.print("Saída: ");
         return b;
-    }
-
-    
-
-    public int triplicaNaoEquivalente(int n){
-        System.out.println("\n\n-----------PROGRAMA---RECUSIVO-----------\n\n");
-
-        int a = n;
-        int b = 0;
-
-        if (n!= 0){
-            a--;
-            b = triplica(a);
-            b++;
-            b++;
-            b--;
-            b++;
-            b++;
-
-        } 
-
-        System.out.print("Saída: ");
-        return b;
-    }
+    }    
 }
   
